7ef11eb3aa9ed0d7d2fe61c88e27a5aa193353a2
Vlad Brukh <48629069+VladBrukh@users.noreply.github.com>
2024-01-16 13:36:16 +0300

    Update Main.java

--- src/main/java/com/vladimirbrukh/Main.java
+++ src/main/java/com/vladimirbrukh/Main.java
@@ -1,6 +1,6 @@
 package com.vladimirbrukh;
 
-private static String msg = ""Hello, qa_guru!!!!!"";
+private static String msg = "Hello, qa_guru!";
 
 public class Main {
     public static void main(String[] args) {
