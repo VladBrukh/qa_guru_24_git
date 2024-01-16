5ded001d560a05e7a179ec24b6b3acd0df1fe744
Vlad Brukh <48629069+VladBrukh@users.noreply.github.com>
2024-01-16 12:57:43 +0300

    Update Main.java

--- src/main/java/com/vladimirbrukh/Main.java
+++ src/main/java/com/vladimirbrukh/Main.java
@@ -2,6 +2,6 @@ package com.vladimirbrukh;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qa_guru!");
+        System.out.println("Hello, qa_guru!!!!!");
     }
 }
